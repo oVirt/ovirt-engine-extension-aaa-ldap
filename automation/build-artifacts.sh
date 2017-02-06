be149334688677c8a8e83e69cee3558afc2ad1cb
Martin Perina <mperina@redhat.com>
2017-02-06 10:14:36 +0100

    packaging: Prepare scripts for standard CI buils
    Prepares scripts from standard CI buils according to
    http://ovirt-infra-docs.readthedocs.io/en/latest/CI/Build_and_test_standards.html
    
    Change-Id: I143a99605a8eed5c00911594daaa8c2e56b07f34
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ automation/build-artifacts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+# Execute build
+./automation/check-patch.sh
+
+# Store any relevant artifacts in exported-artifacts for the ci system to
+# archive
+[[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+find output -iname \*rpm -exec mv "{}" exported-artifacts/ \;
+mv ./*tar.gz exported-artifacts/
