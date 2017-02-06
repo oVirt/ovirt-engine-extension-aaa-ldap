b5d5a03326019fe930d65be696269567cc909897
Martin Perina <mperina@redhat.com>
2017-02-06 10:14:36 +0100

    packaging: Prepare scripts for standard CI buils
    Prepares scripts from standard CI buils according to
    http://ovirt-infra-docs.readthedocs.io/en/latest/CI/Build_and_test_standards.html
    
    Change-Id: I143a99605a8eed5c00911594daaa8c2e56b07f34
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ automation/check-merged.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+
+# At the moment we don't need check-merged, because on patch merging we
+# just build RPMs and publish them and those actions are executed in
+# build-artifacts
+
+echo
