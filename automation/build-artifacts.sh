5184c4144a2d9080fd8a43045bd2cf7047e44422
Martin Perina <mperina@redhat.com>
2017-02-09 10:06:58 +0100

    packaging: Build RPMs during check-patch
    Change-Id: Iaa81d0570f544bdd576192b1b2043f6e52e21d01
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,7 +1,31 @@
 #!/bin/bash -xe
 
-# Execute build
-./automation/check-patch.sh
+SUFFIX=".git$(git rev-parse --short HEAD)"
+
+# Remove any previous artifacts
+rm -rf output
+rm -f ./*tar.gz
+make clean
+
+# Get the tarball
+make dist
+
+# Create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    -ts ./*.gz
+
+# Install any build requirements
+yum-builddep output/*src.rpm
+
+# Build RPMs
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    --rebuild output/*.src.rpm
 
 # Store any relevant artifacts in exported-artifacts for the ci system to
 # archive
