e415486c09d369f32a8d5e00c9018bc9e4f25ac6
Jean-Louis Dupond <jean-louis@dupond.be>
2026-07-13 16:31:45 +0200

    build: update build flow
    - Update the way version tags are added during build.
      Now we can tag a commit in github, and the correct version string will
      be used for the builds.
    - Run the workflow on any tag, so it builds tags in branches also.
    - Drop copr
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- .automation/build-rpm.sh
+++ .automation/build-rpm.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -xe
 
-source $(dirname "$(readlink -f "$0")")/build-srpm.sh
+source "$(dirname "$(readlink -f "$0")")/build-srpm.sh"
 
 # Install build dependencies
 dnf builddep -y rpmbuild/SRPMS/*src.rpm
 
 # Build binary package
+[[ -n "${RELEASE_SUFFIX}" ]] && RELEASE_SUFFIX_ARGS=(--define "release_suffix ${RELEASE_SUFFIX}") || RELEASE_SUFFIX_ARGS=()
 rpmbuild \
-    --define "_topmdir rpmbuild" \
-    --define "_rpmdir rpmbuild" \
+    --define "_topdir $(pwd)/rpmbuild" \
+    --define "_rpmdir $(pwd)/rpmbuild" \
+    "${RELEASE_SUFFIX_ARGS[@]}" \
     --rebuild rpmbuild/SRPMS/*src.rpm
 
 # Move RPMs to exported artifacts
-[[ -d $ARTIFACTS_DIR ]] || mkdir -p $ARTIFACTS_DIR
-find rpmbuild -iname \*rpm | xargs mv -t $ARTIFACTS_DIR
+[[ -d "$ARTIFACTS_DIR" ]] || mkdir -p "$ARTIFACTS_DIR"
+find rpmbuild -iname \*rpm | xargs mv -t "$ARTIFACTS_DIR"
